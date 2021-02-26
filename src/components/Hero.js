63659df11a08a3bd9a56c140778a42e5879d8932
serii <seriiburduja@gmail.com>
2021-02-27 01:11:44 +0200

    hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,39 +1,48 @@
-import React from "react"
-import Image from "gatsby-image"
-import {Link} from "gatsby"
-import {graphql, useStaticQuery} from "gatsby"
-import SocialLinks from "../constants/socialLinks"
+import React from "react";
+import Image from "gatsby-image";
+import { Link } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
+import SocialLinks from "../constants/socialLinks";
+import styles from "./Hero.module.scss";
 // ...GatsbyImageSharpFluid
 const query = graphql`
-{
-    file(relativePath: {eq: "hero-img.png"}) {
+  {
+    file(relativePath: { eq: "hero-img.png" }) {
       childImageSharp {
         fluid {
-         ...GatsbyImageSharpFluid
+          ...GatsbyImageSharpFluid
         }
       }
     }
   }
-`
+`;
 const Hero = () => {
-  const {file: {childImageSharp: {fluid}}} = useStaticQuery(query)
+  const {
+    file: {
+      childImageSharp: { fluid },
+    },
+  } = useStaticQuery(query);
   return (
     <div>
-      <div className="hero">
-        <div className="section-center hero-center">
-          <article className="hero-info">
+      <div className={styles.hero}>
+        <div className={styles.heroCenter}>
+          <article className={styles.heroInfo}>
             <div>
               <div className="underline"></div>
               <h1>I am John</h1>
               <h4>Freelance, web and UI designer</h4>
-              <Link to="/contact" className="btn">Contact me</Link>
-              <SocialLinks/>
+              <Link to="/contact" className="btn">
+                Contact me
+              </Link>
+              <SocialLinks />
             </div>
           </article>
-          <Image fluid={fluid} className="hero-img"/>
+          <div className={styles.heroImg}>
+            <Image fluid={fluid} />
+          </div>
         </div>
       </div>
     </div>
-  )
-}
-export default Hero
+  );
+};
+export default Hero;
